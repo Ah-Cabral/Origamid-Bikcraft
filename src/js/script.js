564beec1cb90af862d9e3c7bb0034b133128fea3
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-22 16:36:36 -0300

    Adicionando condicional if

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        time: 8000, // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: 'fotos', // nome do atributo data-slide="principal"
+        time: 8000, // tempo de transição dos slides
+        nav: true, // se deve ou não mostrar a navegação
+    });
+}
